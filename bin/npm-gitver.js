0e1cb3e4bed50da782b2197cd8d5d940a7b24bd5
TheBookKnight <cadav001@umn.edu>
2025-04-29 00:50:26 -0400

    7 - Implement minimist for input args

--- bin/npm-gitver.js
+++ bin/npm-gitver.js
@@ -1,22 +1,14 @@
 #!/usr/bin/env node
-
+const minimist = require('minimist');
 const { generateGitVersion } = require('../index');
 
 // argument parser
-const args = process.argv.slice(2);
+const args = minimist(process.argv.slice(2), {
+    alias: { f: 'file', b: 'branch' },
+    boolean: ['branch'],
+    string: ['file'],
+    default: { file: './package.json', branch: false },
+});
 
-let filePath = './package.json'; // default file path to package.json
-let includeBranch = false;        // whether to include branch name
-
-for (let i = 0; i < args.length; i++) {
-    if (args[i] === '--file' || args[i] === '-f') {
-        filePath = args[i + 1];
-        i++;
-    }
-    if (args[i] === '--branch' || args[i] === '-b') {
-        includeBranch = true;
-    }
-}
-
-const version = generateGitVersion(filePath, { includeBranch });
+const version = generateGitVersion(args.file, { includeBranch: args.branch });
 console.log(version);
